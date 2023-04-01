c60428f13825712c52cbd4bb7b0e709cc7c0cceb
DanieleMarino70 <danielaotitan@gmail.com>
2023-04-01 22:42:38 +0200

    add: migration songs and reset app scss

---
+++ database/migrations/2023_04_01_203549_create_songs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('album', 100);
+            $table->string('author', 100);
+            $table->string('editor', 100);
+            $table->string('album', 100);
+            $table->time('length');
+            $table->string('poster', 100);
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+};
