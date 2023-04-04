b07465fcbae083bf48fa6f74af0df2ae3ceea2ee
DanieleMarino70 <danielaotitan@gmail.com>
2023-04-04 20:32:08 +0200

    add: old values

--- resources/views/songs/create.blade.php
+++ resources/views/songs/create.blade.php
@@ -20,27 +20,27 @@
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control" id="title" name="title" placeholder="Titolo canzone">
+                <input type="text" class="form-control" id="title" name="title" placeholder="Titolo canzone" value="{{old('title')}}">
             </div>
 
             <div class="mb-3">
                 <label for="album" class="form-label">Album</label>
-                <input type="text" class="form-control" id="album" name="album" placeholder="Nome album">
+                <input type="text" class="form-control" id="album" name="album" placeholder="Nome album" value="{{old('album')}}">
             </div>
 
             <div class="mb-3">
                 <label for="author" class="form-label">Autore</label>
-                <input type="text" class="form-control" id="author" name="author" placeholder="Nome Autore">
+                <input type="text" class="form-control" id="author" name="author" placeholder="Nome Autore" value="{{old('author')}}">
             </div>
 
             <div class="mb-3">
                 <label for="editor" class="form-label">Editore</label>
-                <input type="text" class="form-control" id="editor" name="editor" placeholder="Nome Editore">
+                <input type="text" class="form-control" id="editor" name="editor" placeholder="Nome Editore" value="{{old('editor')}}">
             </div>
 
             <div class="mb-3">
                 <label for="length" class="form-label">Lunghezza brano</label>
-                <input type="number" class="form-control" id="length" name="length" step="0.01" placeholder="Lunghezza in decimale">
+                <input type="number" class="form-control" id="length" name="length" step="0.01" placeholder="Lunghezza in decimale" value="{{old('length')}}">
             </div>
 
             <div class="mb-3">
